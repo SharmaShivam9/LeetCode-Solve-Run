5bd617f3a1886a75ade08c859fda9c3bd610e7e0
SHIVAM SHARMA <137926153+SharmaShivam9@users.noreply.github.com>
2025-11-05 14:21:03 +0530

    Time: 384 ms (70.00%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,1 +1,5 @@
-select distinct author_id as id from views where author_id=viewer_id order by author_id asc;
+select author_id as id 
+from views 
+where author_id=viewer_id
+GROUP BY author_id
+order by author_id asc;
